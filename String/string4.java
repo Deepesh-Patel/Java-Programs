1bfb799f96f157ee05133040942159f779adc9a6
Deepesh Patel <80380848+Deepesh-Patel@users.noreply.github.com>
2021-04-21 10:09:00 +0530

    String_4

---
+++ String/string4.java
@@ -0,0 +1,16 @@
+class string4
+{
+	public static void main(String[] args) 
+	{
+		String a = "Welcome to java";
+		int index;
+		index = a.indexOf('w');
+		System.out.println("index value of w: "+index);
+
+		index = a.indexOf('e');
+		System.out.println("Index value of e: "+index);
+
+		index = a.indexOf('v');
+		System.out.println("Index value of v: "+index);
+	}
+}

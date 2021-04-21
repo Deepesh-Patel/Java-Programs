ed7f870b94587ffb8c89d8e4dafb928b89cebc31
Deepesh Patel <80380848+Deepesh-Patel@users.noreply.github.com>
2021-04-21 08:11:33 +0530

    String_3

---
+++ String/string3.java
@@ -0,0 +1,12 @@
+class string3
+{
+	public static void main(String[] args) 
+	{
+		String a ="Welcome to";
+		String b =" Java";
+		System.out.println("a: "+a);
+		System.out.println("b: "+b);
+		String c = a.concat(b);
+		System.out.println("\na concat b: " +c);
+	}
+}

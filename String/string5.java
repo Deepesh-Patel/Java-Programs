ebb2e0c2cffe50a5a31e86fcbd12f4f6cbcdf14e
Deepesh Patel <80380848+Deepesh-Patel@users.noreply.github.com>
2021-04-22 09:31:48 +0530

    String_5

---
+++ String/string5.java
@@ -0,0 +1,14 @@
+class string5
+{
+	public static void main(String[] args) 
+	{
+		String a= "WELCOME";
+		String b= "java";
+		String c= "GoOd";
+
+		System.out.println(a.toLowerCase());
+		System.out.println(b.toUpperCase());
+		System.out.println(c.toLowerCase());
+		System.out.println(c.toUpperCase());
+	}
+}

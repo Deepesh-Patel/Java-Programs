6d6203014a4d6802c9f7181be30753e1198ff059
Deepesh Patel <80380848+Deepesh-Patel@users.noreply.github.com>
2021-04-22 10:31:36 +0530

    Right_Triangle_Pattern

---
+++ Pattern_Printing/triangle.java
@@ -0,0 +1,15 @@
+class triangle
+{
+	public static void main(String[] args) 
+	{
+		int i, j, r=6;
+		for(i=0; i<r; i++)
+		{
+			for(j=0; j<=i; j++)
+			{
+				System.out.print("* ");
+			}
+			System.out.println();
+		}
+	}
+}

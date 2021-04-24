cc4c081800e542156fc497f158f08c25d2b369ef
Deepesh Patel <80380848+Deepesh-Patel@users.noreply.github.com>
2021-04-24 12:00:06 +0530

    String_6

---
+++ String/string6.java
@@ -0,0 +1,14 @@
+class string6
+{
+	public static void main(String[] args) 
+	{
+		String a="Welcome";
+		String b="Java";
+
+		System.out.println(a);
+		System.out.println(b);
+
+		boolean c=a.equals(b);
+		System.out.println(c);
+	}
+}
